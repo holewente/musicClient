f45246a2a8c799632354285f58d9047a06d18326
Your Name <you@example.com>
2023-01-11 12:21:52 +0100

    'stb'

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,45 @@
 import './App.css';
+import React,{useState} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import {About} from './components/About'
+import {Home} from './components/Home'
+import { Products } from './components/Products';
+import { Contacts } from './components/Contacts';
+import {Routes,Route} from 'react-router-dom'
+import { MyNavbar } from './components/MyNavbar';
+import {Product} from './components/Product'
+import { Login } from './components/Login';
+import { Register } from './components/Register'
+import {UserProfile} from './components/UserProfile'
+import {QueryClient,QueryClientProvider} from 'react-query'
+import {Books} from './components/Books'
+
+const queryClient = new QueryClient()
 
 function App() {
+  const [loggedInUser,setLoggedInUser]=useState({})
+  console.log(loggedInUser,"loggedInUser")
   return (
-    <div className="App">
-      <h1>Test</h1>
+    <QueryClientProvider client={queryClient}>
+      <MyNavbar loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser}/>
+      <div className='holder d-flex justify-content-center'>
+      <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='about' element={<About/>} />
+      <Route path='contacts' element={<Contacts/>} />
+      <Route path='products' element={<Products/>} />
+      <Route path='products/:id' element={<Product/>} />
+      <Route path='login' element={<Login setLoggedInUser={setLoggedInUser}/>} />
+      <Route path='register' element={<Register/>} />
+      {loggedInUser?.username && 
+      <Route path='userProfile' 
+      element={<UserProfile loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser}/>}/>}
+      {loggedInUser?.role=='admin' &&
+      <Route path='books' element={<Books/>}/>}
+      </Routes>
     </div>
+    </QueryClientProvider>
   );
 }
 
---
+++ src/components/FileDrop.jsx
@@ -0,0 +1,25 @@
+import React,{useState} from 'react'
+import { FileUploader } from 'react-drag-drop-files'
+
+const fileTypes=["JPEG","PNG","GIF","JPG"]
+
+export const FileDrop=({setSelFile})=> {
+    const [file, setFile] = useState(null);
+    
+    const handleChange = (file) => {
+    setFile(file);
+    setSelFile(file)
+  };
+  return (
+    <div className="filedrop-holder">
+      <h1>Drag & Drop Avatar</h1>
+      <FileUploader
+        handleChange={handleChange}
+        name="file"
+        types={fileTypes}
+        maxSize="1"
+      />
+      <p>{file ? `File name: ${file.name}` : "no files uploaded yet"}</p>
+    </div>
+  );
+}

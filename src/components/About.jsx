d89a60c9dbe29fee40415085cfee6c0cab8c43b8
Levi <you@example.com>
2023-01-19 11:46:49 +0100

    s

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
-import {motion} from 'framer-motion'
+import React from "react";
+import { motion } from "framer-motion";
 
-export const About=()=> {
+export const About = () => {
   return (
     <motion.div
-    initial={{opacity:0}}
-    animate={{opacity:1}}
-    transition={{delay:0.3,duration:1.5}}>
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.3, duration: 1.5 }}
+    >
       <br />
       <br />
       <br />
-     <h1 className='text-center'>About page</h1>
-     <p className='text-center'>Spoticloud is a Professional Music Streaming Platform. <br />
-      Here we will provide you only interesting content, which you will like very much. <br />
-      We're dedicated to providing you the best of Music Streaming, with a focus on dependability and Revolutionary. <br />
-      We're working to turn our passion for Music Streaming into a booming online website. <br />
-      <b>We hope you enjoy our Music Streaming as much as we enjoy offering them to you.</b> </p>
-     
+      <div className="aboutkeret">
+        <h1 className="text-center">About page</h1>
+        <p className="text-center">
+          Spoticloud is a Professional Music Streaming Platform. <br />
+          Here we will provide you only interesting content, which you will like
+          very much. <br />
+          We're dedicated to providing you the best of Music Streaming, with a
+          focus on dependability and Revolutionary. <br />
+          We're working to turn our passion for Music Streaming into a booming
+          online website. <br />
+          <b>
+            We hope you enjoy our Music Streaming as much as we enjoy offering
+            them to you.
+          </b>{" "}
+        </p>
+      </div>
     </motion.div>
-  )
-}
+  );
+};
--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,26 +1,48 @@
-import React from 'react'
-import {motion} from 'framer-motion'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { motion } from "framer-motion";
+import { NavLink } from "react-router-dom";
 
-export const Contacts=()=> {
+export const Contacts = () => {
   return (
     <motion.div
-    initial={{opacity:0}}
-    animate={{opacity:1}}
-    transition={{delay:0.3,duration:1.5}}>
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.3, duration: 1.5 }}
+    >
       <br />
       <br />
       <br />
-      <h1 className='text-center'>Developers:</h1>
-      <p className='text-center'><a href='https://github.com/holewente'><b>Horváth Levente</b></a><br /><br />
-      <a href='https://github.com/milanszabo2001'><b>Szabó Milán</b></a></p><br />
+      <div className="contactkeret">
+        <h1 className="text-center">Developers:</h1>
+        <p className="text-center">
+          <a href="https://github.com/holewente">
+            <b>Horváth Levente</b>
+          </a>
+          <br />
+          <br />
+          <a href="https://github.com/milanszabo2001">
+            <b>Szabó Milán</b>
+          </a>
+        </p>
+        <br />
 
-     <h2 className='text-center'>Copyright Disclaimer Under Section 107 of the Copyright Act 1976</h2> 
-<p className='text-center'>Copyright Disclaimer Under Section 107 of the Copyright Act 1976, allowance is made for "fair use" for purposes such as criticism, 
-comment, news reporting, teaching, scholarship, and research.</p> 
- <p className='text-center'>Fair use is a use permitted by copyright statute that might otherwise be infringing.</p> 
- <p className='text-center'>Non-profit, educational or personal use tips the balance in favor of fair use.</p> 
-     
+        <h2 className="text-center">
+          Copyright Disclaimer Under Section 107 of the Copyright Act 1976
+        </h2>
+        <p className="text-center">
+          Copyright Disclaimer Under Section 107 of the Copyright Act 1976,
+          allowance is made for "fair use" for purposes such as criticism,
+          comment, news reporting, teaching, scholarship, and research.
+        </p>
+        <p className="text-center">
+          Fair use is a use permitted by copyright statute that might otherwise
+          be infringing.
+        </p>
+        <p className="text-center">
+          Non-profit, educational or personal use tips the balance in favor of
+          fair use.
+        </p>
+      </div>
     </motion.div>
-    )
-}
+  );
+};

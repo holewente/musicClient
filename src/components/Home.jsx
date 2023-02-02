8a907a30c1cddf510543cf431b057722279005c2
Your Name <you@example.com>
2023-02-02 13:19:23 +0100

    'minim'

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import {motion} from 'framer-motion';
 
 export const Home=()=> {
   return (
-    <div className='text-center'>
+    <div className='text-center loginregisterpanel'>
       <motion.h1 initial={{y:-250}} animate={{y:60}}>
         Home page <br />
         <div className='howckep'>

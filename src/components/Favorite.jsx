14fc02147fa2b68d6212ea976615f55d77b76ddf
Levi <you@example.com>
2023-04-14 12:02:35 +0200

    commit

--- src/components/Favorite.jsx
+++ src/components/Favorite.jsx
@@ -49,7 +49,7 @@ export const Favorite=({user_id})=> {
         
          {status=='success' && data.data.map(obj=>
          <span key={obj.id}>
-          <h4 className='cimsor mt-5'>{obj.title} <i class="fa-regular fa-square-minus" onClick={()=>delFav(obj.url)}></i></h4>
+          <h4 className='cimsor mt-5'>{obj.title} <i class="fa-regular fa-square-minus" onClick={()=>mutationDel.mutate(obj.id)}></i></h4>
         
           {obj.link_url.includes('spotify') ? 
           <SpotifyPlayer uri={obj.link_url}
